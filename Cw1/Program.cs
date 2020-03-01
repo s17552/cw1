00eedb8623b0edae36c3c2bcb67a653ec852eeaf
yaroslavchuiev <yaroslav.chuiev@outlook.com>
2020-03-01 17:47:20 +0100

    Add git ignore

--- Cw1/Program.cs
+++ Cw1/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach(var a in args)
+            {
+                Console.WriteLine(a);
+            }
+            var emails = await GetEmails(args[0]);
+
+            foreach (var a in emails)
+            {
+                Console.WriteLine(a);
+            }
+        }
+
+        static async Task<IList<string>> GetEmails(string url)
+        {
+            var httpClient = new HttpClient();
+            var listOfEmails = new List<String>();
+            var response = await httpClient.GetAsync(url);
+
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*",
+            RegexOptions.IgnoreCase);
+            MatchCollection emailMatches = emailRegex.Matches(response.Content.ReadAsStringAsync().Result);
+
+            foreach(var emailMatch in emailMatches)
+            {
+                listOfEmails.Add(emailMatch.ToString());
+            }
+            
+            return listOfEmails;
+
         }
     }
 }
